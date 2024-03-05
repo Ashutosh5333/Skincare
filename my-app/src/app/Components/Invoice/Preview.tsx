5da38ba97130209ca0990ecea76be8683951de0f
Ashutosh lakshakar <lakshakarashutosh@gmail.com>
2024-03-05 10:54:26 +0530

    feat: updated css

--- my-app/src/app/Components/Invoice/Preview.tsx
+++ my-app/src/app/Components/Invoice/Preview.tsx
@@ -7,22 +7,22 @@ import PreviewList from "./PreviewList";
 const Preview = () => {
   return (
     <>
-      <div className="rounded-lg border-2 bg-[#FFFFFF]  shadow-xl px-2 py-10    border-yellow-500">
+      <div className="rounded-lg  bg-[#FFFFFF]  shadow-xl px-2 py-10    border-yellow-500">
         
-        <div className=" px-2 py-2 flex justify-between">
+        <div className="flex pb-6 justify-between">
           <div className="flex items-center">
-            <div className="p-2 px-4 font-normal rounded-lg flex items-center">
+            <div className="p-1 px-2 font-semibold rounded-lg flex items-center">
               Preview Invoice
             </div>
           </div>
           <div className="flex  items-center">
-            <div className="flex flex-row gap-2 text-[#4C4D4F] text-[.6rem] bg-[#F8F8F8] p-2 rounded-lg mr-2">
+            <div className="flex flex-row gap-2 text-[#4C4D4F] text-[.6rem] bg-[#F8F8F8] p-3 rounded-lg mr-2">
               <BsPrinter className="m-auto text-2xl" />
             </div>
           </div>
         </div>
 
-        <div className=" px-2 border-gray-300 rounded-xl">
+        <div className="border-2 px-2 pt-4 border-gray-300 rounded-xl">
           <div className="py-4 px-2">
             <Image
               src={"/assets/Skinclub.svg"}
@@ -31,20 +31,22 @@ const Preview = () => {
               height={100}
             />
           </div>
-          <div className=" py-4 px-2">
-            <p> Summary ----------------- </p>
+         
+          <div className="divider  py-2 flex justify-center items-center">
+            <p className="mx-2 text-[.8rem] text-[#4C4D4F] text-start">SUMMERY</p>
+            <hr className="w-[1000px] border" />
           </div>
 
-          <div className="border-2 px-2 flex justify-between ">
-            <p className="text-[.8rem]"> Dr. Vihang </p>
-            <p className="text-[.8rem]"> Jane Doe </p>
+          <div className="px-2 pt-4 flex justify-between ">
+            <p className="font-semibold text-[.9rem]"> Dr. Vihang </p>
+            <p className="font-semibold pr-20 text-[.9rem]"> Jane Doe </p>
             <p> </p>
           </div>
 
           {/*  */}
 
-          <div>
-            <PreviewList/>
+          <div className="TableList">
+            <PreviewList />
           </div>
 
           <div className=" border-black py-4 px-4 flex  justify-between">
@@ -53,24 +55,24 @@ const Preview = () => {
             <div className="grid  grid-cols-2 gap-4">
               <div className=" text-[.8rem]">
                 <p className="text-end py-1 font-semibold">Subtotal:</p>
-                <p className="text-end text-[#4C4D4F] py-1">Tax (5%):</p>
-                <p className="text-end text-[#4C4D4F] py-1">Discount (10%):</p>
+                <p className="text-end font-semibold text-[#777777] py-1">Tax (5%):</p>
+                <p className="text-end font-semibold text-[#777777] py-1">Discount (10%):</p>
               </div>
               <div className="text-[.8rem] text-end">
                 <p className="py-1 font-semibold">$540.00</p>
-                <p className="py-1 text-[#4C4D4F]">$00.00</p>
-                <p className="py-1 text-[#4C4D4F]">$00.00</p>
-               </div>
+                <p className="py-1 font-semibold text-[#777777]">$00.00</p>
+                <p className="py-1 font-semibold text-[#777777]">$00.00</p>
+              </div>
             </div>
           </div>
 
-            <div className="border-t-2  py-2 "/>
+          <div className="border-t-2  py-2 " />
 
-          <div className=" border-black px-4 flex  justify-between">
+          <div className="pb-2 border-black px-4 flex  justify-between">
             <div></div>
             <div className="grid  grid-cols-2 gap-10">
               <div className="text-start text-[.8rem]">
-                <p className="py-1 font-semibold">Total:</p>
+                <p className="py-1 font-semibold">total</p>
               </div>
               <div className="text-[.8rem] text-end">
                 <p className="py-1 font-semibold">$540.00</p>
@@ -78,21 +80,23 @@ const Preview = () => {
             </div>
           </div>
 
-
-        <div className="px-4 py-4 bg-[#F8F8F8]">
-            <h2 className="text-[.9rem]"> Notes/Terms </h2>
-             <p className="text-[.7rem] text-[#000000]"> Lorem ipsum dolor sit amet. Sit eaque assumenda qui. </p>
-        </div>
-
-        </div>
-
-          <div className=" py-6 flex items-center ">
-
-            <button className="bg-black px-8 rounded-full text-base text-[#ffffff] py-6 text-center m-auto"> Create New Invoice </button>
-
+          <div className="px-4 py-4 mb-5 bg-[#F8F8F8]">
+            <h2 className="text-[.9rem] font-semibold"> Notes/Terms </h2>
+            <p className="text-[.7rem] text-[#777777]">
+              {" "}
+              Lorem ipsum dolor sit amet. Sit eaque assumenda qui.{" "}
+            </p>
           </div>
+        </div>
 
-
+        <div className=" py-4 mt-5 flex items-center ">
+          <button className="bg-black px-8 rounded-full text-base 
+          text-[#ffffff] py-4 text-center font-normal m-auto">
+            {" "}
+            Create New Invoice{" "}
+          </button>
+        </div>
+        
       </div>
     </>
   );
--- my-app/src/app/Components/Invoice/PreviewList.tsx
+++ my-app/src/app/Components/Invoice/PreviewList.tsx
@@ -6,30 +6,30 @@ const PreviewList = () => {
 
   return (
     <div>
-          <div className="border-2 rounded-lg mt-10  py-2 overflow-x-auto">
+          <div className="border-2 rounded-lg mt-10   overflow-x-auto">
             <table className="table-auto w-full  ">
               <thead>
                 <tr className="rounded-t-lg bg-[#000000] ">
                   {INVOICE_HEAD.map((header, index) => (
                     <th
                       key={index}
-                      className="px-4  text-[#FFFFFF] text-[.6rem] text-start py-4"
+                      className="px-4 font-normal text-[#FFFFFF] text-[.8rem] text-start py-4"
                     >
                       {header}
                     </th>
                   ))}
                 </tr>
               </thead>
-              <tbody className="  text-left text-sm  text-[#4C4D4F] w-[90%] m-auto">
+              <tbody className="  text-left text-sm  text-[#000000] w-[90%] m-auto">
                 {INVOICE_ROWS.map((row, rowIndex) => (
                   <tr
                     key={rowIndex}
-                    className=" text-[.7rem] bg-[#F8F8F8] px-8 py-8 "
+                    className="font-semibold  text-[.6rem] bg-[#F8F8F8] px-8 py-8 "
                   >
-                    <td className=" px-4 py-2">{row.name}</td>
-                    <td className=" px-4 py-2">{row.Rate}</td>
-                    <td className=" px-4 py-2">{row.qty}</td>
-                    <td className=" px-4 py-2">{row.Total}</td>
+                    <td className="  px-4 py-2">{row.name}</td>
+                    <td className="  px-4 py-2">{row.Rate}</td>
+                    <td className="  px-4 py-2">{row.qty}</td>
+                    <td className="  px-4 py-2">{row.Total}</td>
                   </tr>
                 ))}
               </tbody>
